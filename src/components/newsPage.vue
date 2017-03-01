13860cc34884d3779fd56c849343863ef5ff1080
Adam Kozuch <adam@cmatsudas-MacBook-Air.local>
2017-03-01 13:15:15 +0900

    Initial commit

---
+++ src/components/newsPage.vue
@@ -0,0 +1,16 @@
+<template id="news">
+  <v-ons-page>
+    <custom-toolbar :title="'News'" :action="toggleMenu"></custom-toolbar>
+    <p style="text-align: center">
+      Some news here.
+    </p>
+  </v-ons-page>
+</template>
+
+<script>
+  import customToolbar from './toolbar'
+  export default {
+    props: ['toggleMenu'],
+    components:{customToolbar}
+  }
+</script>
